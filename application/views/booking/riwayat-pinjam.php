a8f5fd054b3ccafe0e51b8a48a46ba1247d6fef4
AndikaPermanaSidiq01 <andikapermanasidiq00@gmail.com>
2021-06-01 08:14:29 +0700

    memperbaiki bug button booking ulang dari riwayat pinjam

--- application/views/booking/riwayat-pinjam.php
+++ application/views/booking/riwayat-pinjam.php
@@ -21,7 +21,7 @@
           Kosong
         </a>
       <?php else : ?>
-        <a href="<?= base_url('booking/tambahBooking/' . $id); ?>" class="btn-buku booking konfirmasiBooking">
+        <a href="<?= base_url('booking/tambahBooking/' . $rp['id']); ?>" class="btn-buku booking konfirmasiBooking">
           <svg class="mr-2" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path>
             <line x1="3" y1="6" x2="21" y2="6"></line>
